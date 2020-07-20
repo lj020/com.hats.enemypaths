057e5241af85b7c80120e573f128f08ba1cd35e7
Lena <lena.jordan@gmx.de>
2020-07-20 23:20:55 +0200

    Add EnemyPath component and EnemyPathDraw script

---
+++ Editor/DrawEnemyPath.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEditor;
+
+using UnityEngine;
+
+[CustomEditor(typeof(EnemyPath))]
+[CanEditMultipleObjects]
+public class DrawEnemyPath : Editor
+{
+    private EnemyPath enemyPath;
+
+    private void OnSceneGUI()
+    {
+        enemyPath = target as EnemyPath;
+
+        if (enemyPath == null || enemyPath.WayPoints == null || enemyPath.WayPoints.Count <= 1)
+        {
+            return;
+        }
+
+        using (new Handles.DrawingScope(Matrix4x4.Translate(enemyPath.transform.position)))
+        {
+            Handles.DrawPolyLine(enemyPath.WayPoints.ToArray());
+
+            for (var index = 0; index < enemyPath.WayPoints.Count; index++)
+            {
+                enemyPath.WayPoints[index] = Handles.PositionHandle(enemyPath.WayPoints[index], Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Runtime/EnemyPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField]
+    private List<Vector3> wayPoints;
+
+    public List<Vector3> WayPoints => wayPoints ?? (wayPoints = new List<Vector3>());
+}
